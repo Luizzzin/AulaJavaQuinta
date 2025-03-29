6123e269f6fe484262844005497c9c364f05b251
Luizzzin <luizhenribarbodias@gmail.com>
2025-03-29 10:28:15 -0300

    feat:teste de entrada de dados

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,59 @@
 import javax.swing.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
+// entrada de dados via teclado
+//        String inputSenaiJava = JOptionPane.showInputDialog("??");
+//        int input = Integer.parseInt(inputSenaiJava);
+//        input++;
+//----------------------------------------------------------------
+        //esse tambem>
 
-        String inputSenaiJava = JOptionPane.showInputDialog("??");
-        int input = Integer.parseInt(inputSenaiJava);
-        input++;
+//        String nome = JOptionPane.showInputDialog("Digite seu nome:");
+//        JOptionPane.showMessageDialog(null, "Olá, " + nome + "!");
+//--------------------------------------------------------------
+//        String input = JOptionPane.showInputDialog("Digite um número:"); // o painel chama a biblioteca depois a função de abrir uma caixa de dialogo
+//        int numero = Integer.parseInt(input);// aqui ele vai abrir uma caixinha de resposta e seta uma variavel para ela e um tipo int.
+//        JOptionPane.showMessageDialog(null, "O número digitado + 1 é: " + (numero+ 1));
+//        //aqui chama biblioteca e depois abre uma caixa de mensagem para exibir a mensagem.
 
+
+        // entrada de escolhar por meio de input
+//        String[] valores = {"aluno1", "aluno2", "aluno3"}; // um array
+//
+//        String Input2 = (String)JOptionPane.showInputDialog(null,
+//
+//                "Esta é uma pergunta?",
+//                "Título da Caixa de Diálogo",
+//                2,
+//                null,
+//                valores,
+//                valores[0]);// é aonde a escolha vai ser setada no começo para aparecer na tela
+
+        // apenas uma caixa de alerta>
+//        JOptionPane.showMessageDialog(
+//
+//                null,
+//                "Não há uma entrada aqui; apenas uma mensagem",
+//                "Título",
+//                0);
+
+
+// pelo console
+      /*  Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite seu nome: ");
+        String nome = scanner.nextLine();
+
+        System.out.println("Olá, " + nome + "!");
+
+        scanner.close();
+        */
+
+
+        
 
     }
 }
