572e981ad748b3cef93d931e8952adc328fb5c49
Luizzzin <luizhenribarbodias@gmail.com>
2025-03-29 09:41:01 -0300

    Teste commit intelij

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        String inputSenaiJava = JOptionPane.showInputDialog("??");
+        int input = Integer.parseInt(inputSenaiJava);
+        input++;
+
+
+    }
+}
